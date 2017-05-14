793a855e50ea2696ca75256f8188d371d87d402b
Pavel Tsybulin <tsybulin@me.com>
2017-05-14 03:13:46 +0300

    [+] ASN1ino support

--- bluno/bluno.ino
+++ bluno/bluno.ino
@@ -1,31 +1,40 @@
-#include "LedMatrix.h"
+#include <ASN1ino.h>
 
-#define READ_BUFSIZE 20
-uint8_t packetbuffer[READ_BUFSIZE + 1] ;
-LedMatrix m ;
+ASN1ino asn1ino(&Serial) ;
 
 void setup() {
-    m.clear() ;
     Serial.begin(115200) ;
+    for (uint8_t pin = 2; pin < 14 ; pin++) {
+        pinMode(pin, OUTPUT) ;
+    }
+    for (uint8_t pin = 2; pin < 14 ; pin++) {
+        digitalWrite(pin, LOW) ;
+    }
+
+    asn1ino.setCommandHadler(commandHandler) ;
 }
 
 void loop() {
-  if (Serial.available()) {
-      Serial.readBytes(packetbuffer, READ_BUFSIZE) ;
+    asn1ino.poll() ;
+}
 
-      // Led
-      if (packetbuffer[0] == 'D') {
-          uint8_t d = (packetbuffer[1] - '0') * 10 + (packetbuffer[2] - '0') ;
-          sprintf((char *)packetbuffer, "LED %02d", d) ;
-          Serial.write(packetbuffer, 6) ;
-          m.digit(d, false) ;
-      } else if(packetbuffer[0] == 'C') {
-          Serial.print(F("CLEAR")) ;
-          m.clear() ;
-      } else if(packetbuffer[0] == 'P') {
-          Serial.print(F("POINT")) ;
-          m.segment(MatrixSegment::H, true) ;
-      }
-  }
+void commandHandler(ASN1ino *asn1Ino, Command_t *cmd) {
+    if (!cmd || !asn1Ino) {
+        return ;
+    }
+
+    if (cmd->command == 'M' && cmd->args && cmd->args->arg2) {
+        pinMode(cmd->args->arg1, *cmd->args->arg2) ;
+        asn1ino.sendCommand('M', cmd->args->arg1, *cmd->args->arg2) ;
+    } else if (cmd->command == 'V' && cmd->args && cmd->args->arg2) {
+        digitalWrite(cmd->args->arg1, *cmd->args->arg2) ;
+        asn1ino.sendCommand('V', cmd->args->arg1, *cmd->args->arg2) ;
+    } else if (cmd->command == 'v' && cmd->args) {
+        byte value = digitalRead(cmd->args->arg1) ;
+        asn1ino.sendCommand('v', cmd->args->arg1, value) ;
+    } else if (cmd->command == 'a' && cmd->args) {
+        byte value = analogRead(cmd->args->arg1) ;
+        asn1ino.sendCommand('a', cmd->args->arg1, value) ;
+    }
 }
 
